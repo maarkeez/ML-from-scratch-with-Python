dcf0923786fb1321c9cfb9359cc59def070088eb
maarkeez <d.marquez.delgado.91@gmail.com>
2020-04-14 22:24:07 +0200

    [01] Load data from CSV
    - Added basic playground

--- 01_load_data_from_csv/playground.py
+++ 01_load_data_from_csv/playground.py
@@ -16,6 +16,41 @@ def load_csv(filename):
     print("\t- Columns: {0}".format(len(dataset[0])))
     return dataset
 
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+def str_column_to_int(dataset, column):
+    class_values = [row[column] for row in dataset]
+    unique = set(class_values)
+    lookup = dict()
+
+    for i, value in enumerate(unique):
+        lookup[value] = i
+
+    for row in dataset:
+        row[column] = lookup[row[column]]
+
+    return lookup
+
 # Load pima dataset
 pima_filename = "datasets/pima-indians-diabetes.data.csv"
 pima_dataset = load_csv(pima_filename)
+print(pima_dataset[0])
+
+# Convert first column to float
+str_column_to_float(pima_dataset, 0)
+print(pima_dataset[0])
+
+
+# Load iris dataset
+iris_filename = "datasets/iris.data.csv"
+iris_dataset = load_csv(iris_filename)
+print(iris_dataset[0])
+
+# Convert 5th column to int
+lookup = str_column_to_int(iris_dataset, 4)
+print(iris_dataset[0])
+# Print keys for that column
+print(lookup)
+
