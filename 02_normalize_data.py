4ccf44be46dbc37f1ab6a06f01cb314bc9395fca
maarkeez <d.marquez.delgado.91@gmail.com>
2020-04-15 20:07:05 +0200

    [02] Scale ML Data
    - Normalize data

---
+++ 02_normalize_data.py
@@ -0,0 +1,18 @@
+from lib import data_loader
+
+dataset = [
+    [50, 30],
+    [20, 90]
+]
+
+print "Dataset:"
+print dataset
+print
+
+minmax = data_loader.dataset_minmax(dataset)
+print "Min, max:"
+print minmax
+print
+
+print "Normalized dataset:"
+print data_loader.normalize_dataset(dataset, minmax)
