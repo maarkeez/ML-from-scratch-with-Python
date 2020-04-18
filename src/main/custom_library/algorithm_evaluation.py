2a78bb416c3618c5a2c223942053e2fae3514d05
maarkeez <d.marquez.delgado.91@gmail.com>
2020-04-18 20:08:09 +0200

    [06] Algorithm Evaluation
    - Classification: Evaluation with cross-fold validation

--- src/main/custom_library/algorithm_evaluation.py
+++ src/main/custom_library/algorithm_evaluation.py
@@ -1,3 +1,5 @@
+import copy
+
 import evaluation_metrics
 import split_dataset
 from src.main.custom_library import column_conversion
@@ -6,9 +8,9 @@ from src.main.custom_library import column_conversion
 # Assumptions:
 # - Last column of the dataset is the output
 # - It's a classification problem "evaluation_metrics.accuracy" is used
-def with_train_test_split(dataset, algorithm, split, *args):
+def classification_with_train_test_split(data_set, algorithm, split, *args):
     output_value_col_index = -1
-    train, test = split_dataset.train_test_split(dataset, split)
+    train, test = split_dataset.train_test_split(data_set, split)
 
     test_set = column_conversion.clear_column(test, output_value_col_index)
 
@@ -17,3 +19,28 @@ def with_train_test_split(dataset, algorithm, split, *args):
 
     accuracy = evaluation_metrics.accuracy(actual, predicted)
     return accuracy
+
+
+# Assumptions:
+# - Last column of the dataset is the output
+# - It's a classification problem "evaluation_metrics.accuracy" is used
+def classification_with_cross_validation(data_set, algorithm, n_folds, *args):
+    output_value_col_index = -1
+    folds = split_dataset.cross_validation_split(data_set, n_folds)
+    scores = list()
+
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+
+        test_set = copy.deepcopy(fold)
+        test_set = column_conversion.clear_column(test_set, output_value_col_index)
+
+        actual = [row[output_value_col_index] for row in fold]
+        predicted = algorithm(train_set, test_set, *args)
+
+        accuracy = evaluation_metrics.accuracy(actual, predicted)
+        scores.append(accuracy)
+
+    return scores
--- test/main/custom_library/test_algorithm_evaluation.py
+++ test/main/custom_library/test_algorithm_evaluation.py
@@ -5,14 +5,11 @@ from src.main.custom_library import api_marchine_learning as api
 
 
 class Test(TestCase):
-    def test_with_train_test_split(self):
+    def test_classification_with_train_test_split(self):
         # seed: is to ensure the exact same split of the data is made every time the code is executed
         seed(1)
 
-        # Load and prepare data
-        pima_dataset = api.load_dataset_pima()
-        for row_index in range(len(pima_dataset[0])):
-            api.str_column_to_float(pima_dataset, row_index)
+        pima_dataset = self.load_and_prepare_dataset_pima()
 
         accuracy = api.algorithm_evaluation_with_train_test_split(
             dataset=pima_dataset,
@@ -21,3 +18,34 @@ class Test(TestCase):
 
         accuracy_round = round(accuracy, 3)
         self.assertEqual(69.055, accuracy_round)
+
+    def test_classification_with_cross_validation(self):
+        # seed: is to ensure the exact same split of the data is made every time the code is executed
+        seed(1)
+
+        expected_scores = [
+            60.130718954248366,
+            62.745098039215684,
+            64.70588235294117,
+            71.89542483660131,
+            66.01307189542483
+        ]
+        pima_dataset = self.load_and_prepare_dataset_pima()
+
+        scores = api.algorithm_evaluation_with_cross_validation(
+            dataset=pima_dataset,
+            algorithm=api.algorithm_classification_zero_rule,
+            n_folds=5
+        )
+        mean_accuracy = (sum(scores) / len(scores))
+        mean_accuracy = round(mean_accuracy, 3)
+
+        self.assertEqual(expected_scores, scores)
+        self.assertEqual(65.098, mean_accuracy)
+
+    def load_and_prepare_dataset_pima(self):
+        pima_dataset = api.load_dataset_pima()
+        for row_index in range(len(pima_dataset[0])):
+            api.str_column_to_float(pima_dataset, row_index)
+
+        return pima_dataset
